bd0cdaf61c1675fed3789d0d2cbcee37d8aa5e90
Shariq Shaukat <shariqshaukat786@gmail.com>
2023-12-15 17:17:09 +0500

    edit admin.py

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.views.decorators.csrf import csrf_exempt
-@csrf_exempt
 
 # Register your models here.
 from .models import Video
@@ -10,6 +9,7 @@ from .models import Comment
 from .models import ImageModel
 from .models import Applicant
 
+@csrf_exempt
 class VideoAdmin(admin.ModelAdmin):
     list_display = ('title', 'course', 'order',)  # Specify the fields you want to display
     list_editable = ('order',)
