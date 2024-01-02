0d25da73d52dfbb985f84e9f5de648bf03da8186
Shariq Shaukat <shariqshaukat786@gmail.com>
2024-01-02 23:49:45 +0500

    added volumes for data and sitemap

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,6 +20,8 @@ from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from myapp import views
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
